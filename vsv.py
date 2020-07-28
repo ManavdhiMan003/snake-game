a6bc87dcbba7fb8e0d3a60b387ef4af6d6b83db8
ManavdhiMan003 <65016580+ManavdhiMan003@users.noreply.github.com>
2020-07-28 20:37:48 +0530

    Add files via upload

---
+++ vsv.py
@@ -0,0 +1,30 @@
+while(true):
+	print('Hit:1 http://dl.google.com/linux/chrome/deb stable InRelease
+Hit:2 http://in.archive.ubuntu.com/ubuntu bionic InRelease                     
+Get:3 http://in.archive.ubuntu.com/ubuntu bionic-updates InRelease [88.7 kB]   
+Get:4 http://packages.microsoft.com/repos/vscode stable InRelease [3,959 B]    
+Get:5 http://in.archive.ubuntu.com/ubuntu bionic-backports InRelease [74.6 kB] 
+Get:6 http://security.ubuntu.com/ubuntu bionic-security InRelease [88.7 kB]    
+Hit:7 http://ppa.launchpad.net/deadsnakes/ppa/ubuntu bionic InRelease          
+Get:8 http://packages.microsoft.com/repos/vscode stable/main amd64 Packages [180 kB]
+Hit:9 https://download.sublimetext.com apt/stable/ InRelease                   
+Get:10 http://in.archive.ubuntu.com/ubuntu bionic-updates/main amd64 Packages [948 kB]
+Hit:11 https://download.sublimetext.com apt/dev/ InRelease                     
+Get:12 http://in.archive.ubuntu.com/ubuntu bionic-updates/main i386 Packages [683 kB]
+Get:13 http://security.ubuntu.com/ubuntu bionic-security/main i386 Packages [472 kB]
+Get:14 http://security.ubuntu.com/ubuntu bionic-security/main amd64 Packages [724 kB]
+Get:15 http://security.ubuntu.com/ubuntu bionic-security/main Translation-en [229 kB]
+Get:16 http://security.ubuntu.com/ubuntu bionic-security/main amd64 DEP-11 Metadata [40.6 kB]
+Get:17 http://security.ubuntu.com/ubuntu bionic-security/restricted amd64 Packages [45.7 kB]
+Get:18 http://security.ubuntu.com/ubuntu bionic-security/restricted Translation-en [11.4 kB]
+Get:19 http://security.ubuntu.com/ubuntu bionic-security/universe amd64 DEP-11 Metadata [42.1 kB]
+Get:20 http://security.ubuntu.com/ubuntu bionic-security/multiverse amd64 DEP-11 Metadata [2,464 B]
+Get:21 http://in.archive.ubuntu.com/ubuntu bionic-updates/main amd64 DEP-11 Metadata [303 kB]
+Get:22 http://in.archive.ubuntu.com/ubuntu bionic-updates/universe amd64 Packages [1,075 kB]
+Get:23 http://in.archive.ubuntu.com/ubuntu bionic-updates/universe i386 Packages [1,017 kB]
+Get:24 http://in.archive.ubuntu.com/ubuntu bionic-updates/universe amd64 DEP-11 Metadata [273 kB]
+Get:25 http://in.archive.ubuntu.com/ubuntu bionic-updates/multiverse amd64 DEP-11 Metadata [2,468 B]
+Get:26 http://in.archive.ubuntu.com/ubuntu bionic-backports/universe amd64 DEP-11 Metadata [7,968 B]
+Err:27 https://repo.skype.com/deb stable InRelease                             
+  504  Gateway Time-out [IP: 104.74.20.179 443]
+Fetched 6,313 kB in 26s (242 kB/s)')
